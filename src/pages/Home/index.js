c184c78be49fecf0df113eca758fb2b87804da47
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-12-23 16:27:15 -0300

    feat: use redux for categories

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import Header from "components/Header";
 import styles from "./Home.module.scss";
 import relogio from "assets/inicial.png";
-import { categorias } from "./Home.constants";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Home() {
   const navigate = useNavigate();
 
+  const categorias = useSelector(state => state.categorias);
+    
   return (
     <div>
       <div>
