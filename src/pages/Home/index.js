ec799658eba404a2a4acef7072a953796dd4d1c0
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-12-23 15:20:37 -0300

    feat: create header component

---
+++ src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import Header from 'components/Header';
+import styles from './Home.module.scss';
+import relogio from 'assets/inicial.png';
+
+export  default function Home() {
+  return (
+    <div>
+      <div>
+        <Header 
+          titulo='Classificados Tech'
+          descricao='Compre diversos tipos de produtos no melhor site do Brasil!'
+          imagem={relogio}
+          className={styles.header}
+        />
+      </div>
+    </div>
+  )
+}
